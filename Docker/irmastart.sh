c3d23bc0f3d1c50de1ec9f186658eb54203303ad
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-02-16 11:30:52 +0100

    Added option to not update schema. Avoids my modifications getting overwritten

--- Docker/irmastart.sh
+++ Docker/irmastart.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-irma server -vvvvvv --no-tls --sse --allow-unsigned-callbacks --url http://145.116.169.159:8088 -l 0.0.0.0 --port 8088
+irma server -vvvvvv --no-tls --sse --allow-unsigned-callbacks --url http://145.116.169.159:8088 -l 0.0.0.0 --port 8088 --schemes-update 0
