d0a31c8e1a509b2608c124f502815fbb07860f81
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-04-12 16:04:23 +0200

    Added code to generate token and relative IRMA card (as QR code)

---
+++ registration-poc/src/irma_session_handler.rs
@@ -0,0 +1,71 @@
+//Code adapted from https://github.com/tweedegolf/irmars/blob/main/examples/issuance.rs
+
+use std::time::Duration;
+use irma::{CredentialBuilder, IrmaClient, IrmaRequest, IssuanceRequestBuilder};
+use tokio::time::sleep;
+
+pub(crate) struct IrmaSessionHandler {
+    client: IrmaClient
+}
+
+impl IrmaSessionHandler{
+    pub(crate) fn new(url: &str) ->IrmaSessionHandler{
+        IrmaSessionHandler{
+            client: IrmaClient::new(url).unwrap()
+        }
+    }
+
+    fn build_request(&self, credential: String, value: String) -> IrmaRequest {
+        let request = IssuanceRequestBuilder::new()
+            .add_credential(
+                CredentialBuilder::new(credential.into())
+                    .attribute("id".into(), value.into())//id hardcoded for now, not even sure about what's supposed to be in there TODO
+                    .build(),
+            )
+            .build();
+        return request;
+    }
+
+    fn generate_qr(&self, sessionptr: String){  //TODO: make this generate an image instead of a string, so that I can embed it into the web page
+        // Render a qr
+        let qr = qrcode::QrCode::new(sessionptr)
+            .unwrap()
+            .render::<char>()
+            .quiet_zone(false)
+            .module_dimensions(2, 1)
+            .build();
+        println!("\n\n{}", qr);
+    }
+
+    #[tokio::main]
+    pub async fn issue_credential(&self, credential: String, value: String){
+        let request=self.build_request(credential, value);
+
+        // Start the session
+        let session = self.client
+            .request(&request)
+            .await
+            .expect("Failed to start session");
+
+        // Encode the session pointer
+        let sessionptr = serde_json::to_string(&session.session_ptr).unwrap();
+        self.generate_qr(sessionptr);
+
+        // Periodically poll if the session was successfully concluded
+        loop {
+            match self.client.result(&session.token).await {
+                Ok(_) => break,
+                Err(irma::Error::SessionNotFinished(_)) => {}
+                Err(v) => panic!("{}", v),
+            }
+
+            sleep(Duration::from_secs(2)).await;
+        }
+
+        println!("Issuance done");
+    }
+
+
+
+}
+
---
+++ registration-poc/src/token_generator.rs
@@ -0,0 +1,9 @@
+use std::ops::Add;
+use uuid::Uuid;
+
+///Generates a unique token for a participant in the form of "participant:UUIDv4".
+pub(crate) fn generate_participant_token() -> String {
+    let id = Uuid::new_v4();
+    let id:String=String::from("participant:").add(&id.to_string());
+return id;
+}
