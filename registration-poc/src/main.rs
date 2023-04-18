1e89eb5ab83fe3df01555b7595a099f710576c52
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-04-18 14:01:12 +0200

    Working version, generates QR code as a String

--- registration-poc/src/main.rs
+++ registration-poc/src/main.rs
@@ -1,12 +1,30 @@
+use rocket::Rocket;
 use crate::irma_session_handler::IrmaSessionHandler;
 use crate::token_generator::generate_participant_token;
+#[macro_use] extern crate rocket;
 
 mod token_generator;
 mod irma_session_handler;
 
-fn main() {
-    let id = generate_participant_token();
-    println!("id: {}", id);
+
+#[get("/")]
+async fn index() -> String {
     let irmaSessionHandler=IrmaSessionHandler::new("http://localhost:8088");
-    irmaSessionHandler.issue_credential("irma-demo.PEP.id".to_string(), id);
+    let id = generate_participant_token();
+    let qr=irmaSessionHandler.issue_credential("irma-demo.PEP.id".to_string(), id).await;
+    return qr;
 }
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
+
+
+
+
+
+
+
+
+
