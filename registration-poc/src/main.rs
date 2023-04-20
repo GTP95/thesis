5587c33a28fea480699fc3919565d89a5df5b1c5
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-04-20 14:33:22 +0200

    Fixed formatting

--- registration-poc/src/main.rs
+++ registration-poc/src/main.rs
@@ -1,19 +1,21 @@
-use std::ops::Add;
-use rocket::response::stream::TextStream;
-use rocket::tokio::time::{sleep, Duration};
 use crate::irma_session_handler::IrmaSessionHandler;
 use crate::token_generator::generate_participant_token;
-#[macro_use] extern crate rocket;
+use rocket::response::stream::TextStream;
+use rocket::tokio::time::{sleep, Duration};
+use std::ops::Add;
+#[macro_use]
+extern crate rocket;
 
-mod token_generator;
 mod irma_session_handler;
-
+mod token_generator;
 
 #[get("/")]
 async fn index() -> TextStream![String] {
-    let irma_session_handler=IrmaSessionHandler::new("http://localhost:8088");   //eduroam ip: 145.116.169.159
+    let irma_session_handler = IrmaSessionHandler::new("http://localhost:8088"); //eduroam ip: 145.116.169.159
     let id = generate_participant_token();
-    let request_result=irma_session_handler.issue_credential("irma-demo.PEP.id".to_string(), &id).await;
+    let request_result = irma_session_handler
+        .issue_credential("irma-demo.PEP.id".to_string(), &id)
+        .await;
 
     TextStream! {
         yield request_result.qr.add("\n\nCredential to be added to auth server: ").add(&id);   //Shows QR code and the credential contained therein
@@ -31,19 +33,9 @@ async fn index() -> TextStream![String] {
 
         yield String::from("\nIssuance done");
     }
-
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
 }
-
-
-
-
-
-
-
-
-
--- registration-poc/src/token_generator.rs
+++ registration-poc/src/token_generator.rs
@@ -4,6 +4,6 @@ use uuid::Uuid;
 ///Generates a unique token for a participant in the form of "participant:UUIDv4".
 pub(crate) fn generate_participant_token() -> String {
     let id = Uuid::new_v4();
-    let id:String=String::from("participant:").add(&id.to_string());
-return id;
+    let id: String = String::from("participant:").add(&id.to_string());
+    return id;
 }
