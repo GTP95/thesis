9655862c774e190056f4cf11edcb877104f9742c
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-06-27 13:05:55 +0200

    Forcing HTTPS and TLSv1.2, still getting
    reqwest::Error { kind: Request, url: Url { scheme: "https", cannot_be_a_base: false, username: "", password: None, host: Some(Domain("localhost")), port: Some(16512), path: "/", query: None, fragment: None }, source: hyper::Error(Parse(Version)) }

--- participant-login-poc/src/https_client.rs
+++ participant-login-poc/src/https_client.rs
@@ -11,7 +11,12 @@ impl HttpsClient {
     pub fn new(url: String, uid_field_name: String, spoof_check_secret: String, root_ca_certificate_path: String)-> HttpsClient {
         let buf= read(root_ca_certificate_path).expect("Error reading root CA certificate");
         let cert = reqwest::Certificate::from_pem(&buf).expect("Error parsing root CA certificate");
-        let client_builder=reqwest::Client::builder().add_root_certificate(cert);
+        let client_builder=reqwest::Client::builder()
+            .add_root_certificate(cert)
+            .https_only(true)
+            //Currently, PEP only supports TLSv1.2, forcing that version
+            .min_tls_version(reqwest::tls::Version::TLS_1_2)
+            .max_tls_version(reqwest::tls::Version::TLS_1_2);
         let client= client_builder.build().expect("Error building HTTPS client");
 
         HttpsClient {
