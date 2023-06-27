c164044efab04cb0e7452f9dce042d6f4ed8d801
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-06-27 14:35:43 +0200

    Added docstrings

--- participant-login-poc/src/https_client.rs
+++ participant-login-poc/src/https_client.rs
@@ -8,6 +8,11 @@ pub struct HttpsClient {
 }
 
 impl HttpsClient {
+    /// Creates a new HTTPS client
+    /// * `url` - The URL to send the authentication request to
+    /// * `uid_field_name` - The name of the HTTP header that contains the user ID
+    /// * `spoof_check_secret` - The secret to use for the Shibboleth spoof check
+    /// * `root_ca_certificate_path` - The path to the root CA certificate, used to verify the authenticity of the server when using self-signed certificates
     pub fn new(url: String, uid_field_name: String, spoof_check_secret: String, root_ca_certificate_path: String)-> HttpsClient {
         let buf= read(root_ca_certificate_path).expect("Error reading root CA certificate");
         let cert = reqwest::Certificate::from_pem(&buf).expect("Error parsing root CA certificate");
@@ -27,6 +32,9 @@ impl HttpsClient {
         }
     }
 
+    /// Sends an authentication request to the server
+    /// * `uid` - The user ID to send in the HTTP header
+    /// * `spoof_check_secret` - The secret to use for the Shibboleth spoof check
     pub async fn send_auth_request(&self, uid: &str, spoof_check_secret: &str) -> Result<reqwest::Response, reqwest::Error> {
         let request = self.client
             .post(&self.url)
