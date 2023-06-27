007c324aa400c0aa3ed4697957d8f10c88454ee0
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-06-27 12:46:49 +0200

    Switched to HTTPS, now I get
    Error: reqwest::Error { kind: Request, url: Url { scheme: "https", cannot_be_a_base: false, username: "", password: None, host: Some(Domain("localhost")), port: Some(16512), path: "/", query: None, fragment: None }, source: hyper::Error(Parse(Version)) }
    
    Maybe wrong TLS version? PEP only suports v1.2

--- participant-login-poc/src/https_client.rs
+++ participant-login-poc/src/https_client.rs
@@ -1,3 +1,5 @@
+use std::fs::read;
+
 pub struct HttpsClient {
     pub client: reqwest::Client,
     pub url: String,
@@ -6,9 +8,14 @@ pub struct HttpsClient {
 }
 
 impl HttpsClient {
-    pub fn new(url: String, uid_field_name: String, spoof_check_secret: String)-> HttpsClient {
+    pub fn new(url: String, uid_field_name: String, spoof_check_secret: String, root_ca_certificate_path: String)-> HttpsClient {
+        let buf= read(root_ca_certificate_path).expect("Error reading root CA certificate");
+        let cert = reqwest::Certificate::from_pem(&buf).expect("Error parsing root CA certificate");
+        let client_builder=reqwest::Client::builder().add_root_certificate(cert);
+        let client= client_builder.build().expect("Error building HTTPS client");
+
         HttpsClient {
-            client: reqwest::Client::new(),
+            client: client,
             url: url,
             uid_field_name: uid_field_name,
             spoof_check_secret: spoof_check_secret
@@ -16,11 +23,11 @@ impl HttpsClient {
     }
 
     pub async fn send_auth_request(&self, uid: &str, spoof_check_secret: &str) -> Result<reqwest::Response, reqwest::Error> {
-        let request = reqwest::Client::new()
+        let request = self.client
             .post(&self.url)
             .header("Shib-Spoof-Check", spoof_check_secret)
             .header(self.uid_field_name.clone(), uid)
-            .body("Just some data to complete message")
+            .body("")
             .send()
             .await?;
         Ok(request)
