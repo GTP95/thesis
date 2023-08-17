69ef8f0722943bcf7a889da4c07c0293a97318fd
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-08-17 22:14:58 +0200

    Application starts, but panics. thread 'main' panicked at 'already borrowed: BorrowMutError' The reason seems to be that I'm asking for a mutable reference to the state inside app and then I'm  doing the same inside other components that I'm calling from app.

--- participant-login-poc/src/main.rs
+++ participant-login-poc/src/main.rs
@@ -6,61 +6,63 @@ use std::error::Error;
 use crate::irma_session_handler::IrmaSessionHandler;
 use crate::http_client::HttpClient;
 use std::fs;
-use std::ops::Add;
-use dioxus::prelude::{Element, Scope};
+use dioxus::html::input;
+use dioxus::prelude::*;
 use irma::{SessionStatus, SessionToken};
 use tera::Tera;
 
-struct State{
+enum CurrentStatus { StartUp, Qr, Success, Error }
+
+struct State {
     config: Config,
     template_engine: TemplateEngine,
     irma_session_handler: IrmaSessionHandler,
-    http_client: HttpClient
+    http_client: HttpClient,
+    current_status: CurrentStatus,
 }
 
 struct Config {
     server_address: String,
     user_id: String,
     spoof_check_secret: String,
-    uid_field_name: String
+    uid_field_name: String,
 }
 
-struct TemplateEngine{
-    tera: Tera
+struct TemplateEngine {
+    tera: Tera,
 }
 
-struct Codes{
+struct Codes {
     code: String,
-    code_verifier: [u8; 32]
+    code_verifier: [u8; 32],
 }
 
 
-async fn irma_disclose_id(template_engine: TemplateEngine) ->  String {
-    let irma_session_handler = IrmaSessionHandler::new("http://localhost:8088");
+async fn irma_disclose_id(template_engine: &TemplateEngine) -> String {
+    let irma_session_handler = IrmaSessionHandler::new("http://localhost:8088");    //TODO: get from global shared state
 
 
     let request_result = irma_session_handler
         .disclose_id(String::from("irma-demo.PEP.id.id"))
         .await;
 
-    let qr= request_result.qr;
+    let qr = request_result.qr;
 
-    //render Tera template showing the qr code
+    //render Tera template showing the Qr code
     let mut context = tera::Context::new();
-    context.insert("qr", &qr);
+    context.insert("Qr", &qr);
     context.insert("session_id", &request_result.session.token.0);
-   template_engine.tera.render("disclose.html", &context).unwrap()
+    template_engine.tera.render("disclose.html", &context).unwrap()
 }
 
 
 async fn get_status(session_id: String, irma_session_handler: IrmaSessionHandler) -> String {
-    let session_token=SessionToken(session_id);
-    let sesion_result =irma_session_handler.get_status(&session_token).await;
+    let session_token = SessionToken(session_id);
+    let sesion_result = irma_session_handler.get_status(&session_token).await;
 
     match sesion_result {
         Ok(session_result) => {
-
-            match  session_result.status {
+            match session_result.status {
                 SessionStatus::Initialized => String::from("Initialized"),
                 SessionStatus::Pairing => String::from("Pairing"),
                 SessionStatus::Connected => String::from("Connected"),
@@ -70,70 +72,39 @@ async fn get_status(session_id: String, irma_session_handler: IrmaSessionHandler
             }
         }
 
-
         Err(error) => {
             error.to_string()
         }
     }
-
 }
 
 
 async fn success(session_id: String, irma_session_handler: IrmaSessionHandler, template_engine: TemplateEngine, config: Config, http_client: HttpClient) -> String {
     let session_token = SessionToken(session_id);
     let session_result = irma_session_handler.get_status(&session_token).await;
-    let disclosed_attribute=session_result.unwrap().disclosed[0][0].clone().raw_value.unwrap(); //TODO: see if this expression can be simplified
-    let request= request_code_for_token(&config.server_address, &disclosed_attribute, &config.spoof_check_secret, &config.uid_field_name, &http_client);
+    let disclosed_attribute = session_result.unwrap().disclosed[0][0].clone().raw_value.unwrap(); //TODO: see if this expression can be simplified
+    let request = request_code_for_token(&config.server_address, &disclosed_attribute, &config.spoof_check_secret, &config.uid_field_name, &http_client);
     let mut context = tera::Context::new();
-    let code_for_token=request.await;
-    match code_for_token {
-        Ok(codes)=> {
+    let code_for_token = request.await;
+    return match code_for_token {
+        Ok(codes) => {
             context.insert("disclosed_attribute", &disclosed_attribute);
             context.insert("code_for_token", &codes.code);
             context.insert("auth_server_base_url", &config.server_address);
             context.insert("code_verifier", &codes.code_verifier);
-            let rendered_html = template_engine.tera.render("success.html", &context).unwrap();
-            return rendered_html;
+            let rendered_html = template_engine.tera.render("Success.html", &context).unwrap();
+            rendered_html
         }
-        Err(error)=>{
+        Err(error) => {
             context.insert("error_message", &error.to_string());
             let rendered_html = template_engine.tera.render("error.html", &context).unwrap();
-            return rendered_html;
+            rendered_html
         }
-    }
-
-
-
-}
-
-
-
-
-
-fn app(cx: Scope)  -> Element {
-
-
-
-}
-
-///Sends an HTTP request to PEP's auth server containing the headers with the disclosed attribute
-async fn request_code_for_token(server_address: &str, user_id: &str, spoof_check_secret: &str, uid_field_name: &str, client: &HttpClient) -> Result<Codes, Box<dyn Error>>{
-
-    let auth_response = client
-        .send_auth_request(&String::from(user_id))
-        .await?;
-    let redirect_url= auth_response.response.headers()["location"].to_str()?;
-    let code_verifier= auth_response.code_verifier;
-    let code=redirect_url.split('=').collect::<Vec<&str>>()[1].to_owned();
-
-    let result=Codes{
-        code,
-        code_verifier
     };
-    Ok(result)
 }
 
-fn main() {
+
+fn app(cx: Scope<'_>) -> Element<'_> {
     //open and parse config.toml configuration file
     let config =
         fs::read_to_string("config/config.toml").expect("Error reading config/config.toml file");
@@ -144,7 +115,7 @@ fn main() {
     let path_to_spoof_check_secret_file = config["path_to_spoof_check_secret_file"]
         .as_str()
         .expect("Error parsing path_to_spoof_check_secret_file from config/config.toml");
-    let path_to_root_ca_certificate=config["path_to_root_ca_certificate"]
+    let path_to_root_ca_certificate = config["path_to_root_ca_certificate"]
         .as_str()
         .expect("Error parsing path_to_root_ca_certificate from config/config.toml");
     let uid_field_name = config["uid_field_name"]
@@ -153,10 +124,10 @@ fn main() {
     let auth_server_address = config["auth_server_address"]
         .as_str()
         .expect("Error parsing auth_server_address from config/config.toml");
-    let uid_field_name= config["uid_field_name"]
+    let uid_field_name = config["uid_field_name"]
         .as_str()
         .expect("Error parsing uid_field_name from config/config.toml");
-    let irma_server_address= config["irma_server_address"]
+    let irma_server_address = config["irma_server_address"]
         .as_str()
         .expect("Error parsing irma_server_address from config/config.toml");
 
@@ -164,7 +135,7 @@ fn main() {
     let spoof_check_secret = fs::read_to_string(path_to_spoof_check_secret_file)
         .expect("Error reading spoof_check_secret file indicated in config/config.toml");
     //remove eventual withespace characters, including newlines
-    let spoof_check_secret= spoof_check_secret.trim().to_string();
+    let spoof_check_secret = spoof_check_secret.trim().to_string();
     //parse Tera templates
     let mut tera = match Tera::new("templates/**/*.html") {
         Ok(t) => t,
@@ -181,16 +152,73 @@ fn main() {
 
     simple_logger::SimpleLogger::new().env().init().unwrap();   //logging, see https://docs.rs/simple_logger/4.2.0/simple_logger/
 
-    let config=Config {
+    let config = Config {
         server_address: String::from(auth_server_address),
         user_id: String::from(uid_field_name),
         spoof_check_secret: spoof_check_secret,
-        uid_field_name: String::from(uid_field_name)
+        uid_field_name: String::from(uid_field_name),
     };
-    let state=State {
+    let status = State {
         config: config,
-        template_engine: TemplateEngine{tera},
+        template_engine: TemplateEngine { tera },
         irma_session_handler: irma_session_handler,
-        http_client: http_client
+        http_client: http_client,
+        current_status: CurrentStatus::StartUp,
     };
+
+    use_shared_state_provider(cx, || status);
+
+    let status=use_shared_state::<State>(cx).unwrap().read();  //Get a new reference since I lost ownership by calling use_shared_state_provider
+
+    match status.current_status {
+        CurrentStatus::StartUp => {
+            render!{startup(cx)}
+        }
+        CurrentStatus::Qr => {
+            render!{qr(cx)}
+        }
+        CurrentStatus::Success => { cx.render(rsx!("Logged in")) }
+        CurrentStatus::Error => { cx.render(rsx!("Error")) }
+    }
+}
+
+pub fn startup(cx: Scope) -> Element{
+    let status=use_shared_state::<State>(cx).unwrap();
+    cx.render(rsx!(button{
+                onclick: move |event| status.write().current_status=CurrentStatus::Qr,
+                "Login with Yivi app"
+            }))
+}
+pub fn qr(cx: Scope) -> Element {
+    let status=use_shared_state::<State>(cx).unwrap();
+    let contents = use_future(
+        cx, (),
+        {to_owned![status]; move |_| async move { irma_disclose_id(&status.read().template_engine).await} }
+
+    ).value().unwrap();
+    cx.render(rsx! {
+    div {
+        dangerous_inner_html: "{contents}"
+    }
+})
+}
+
+///Sends an HTTP request to PEP's auth server containing the headers with the disclosed attribute
+async fn request_code_for_token(server_address: &str, user_id: &str, spoof_check_secret: &str, uid_field_name: &str, client: &HttpClient) -> Result<Codes, Box<dyn Error>> {
+    let auth_response = client
+        .send_auth_request(&String::from(user_id))
+        .await?;
+    let redirect_url = auth_response.response.headers()["location"].to_str()?;
+    let code_verifier = auth_response.code_verifier;
+    let code = redirect_url.split('=').collect::<Vec<&str>>()[1].to_owned();
+
+    let result = Codes {
+        code,
+        code_verifier,
+    };
+    Ok(result)
+}
+
+fn main() {
+    dioxus_desktop::launch(app);
 }
